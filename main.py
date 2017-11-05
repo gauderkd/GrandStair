5558f2021e7061ac528a8c1236cee8ae57e349b6
gauderkd <dragnerz@fdraconis.com>
2017-11-04 23:08:19 -0400

    Wrote new program file, this will be used for the krumhansl pilot but can be extended further later on

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import kivy
 kivy.require('1.10.0') # ToDo: Update
 
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 
+class Controller(BoxLayout):
+	def __init__(self):
+		super(Controller, self).__init__()
+
+	def new_thickness(self, *args):
+		self.lbl.text = str(int(args[1]))
 
 class GrandStairApp(App):
-    pass
-
+    def build(self):
+    	return Controller()
 
 if __name__ == '__main__':
     GrandStairApp().run()
-
